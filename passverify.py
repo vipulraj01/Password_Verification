3aa6b7a1b62691caa368b221deed5cedeaccefec
Vipul <vipulraj011@gmail.com>
2022-10-27 20:12:20 +0530

    initial commit

---
+++ passverify.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"vipulraj01": "123456", "vipulraj02": "000000"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
